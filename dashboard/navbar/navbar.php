fc304dd06570c4e160b76c7df6497ed933297ff8
Denzo <montesdenzel25@gmail.com>
2025-05-10 12:42:38 -0500

    Vistas modulares

---
+++ dashboard/navbar/navbar.php
@@ -0,0 +1,108 @@
+<?php
+  require '../conexion/conexion-BillBot.php'; // Conexión a la base de datos
+
+  /*QUERY PARA FOTO DE PERFIL */
+$sql = "SELECT * FROM usuarios";
+$query2 = mysqli_query($con, $sql);
+?>
+
+<nav class="main-nav--bg">
+    <div class="container main-nav">
+    <div class="main-nav-start">
+      <div class="search-wrapper">
+        
+      </div>
+    </div>
+    <div class="main-nav-end">
+      <button class="sidebar-toggle transparent-btn" title="Menu" type="button">
+        <span class="sr-only">Toggle menu</span>
+        <span class="icon menu-toggle--gray" aria-hidden="true"></span>
+      </button>
+      <div class="lang-switcher-wrapper">
+        <button class="lang-switcher transparent-btn" type="button">
+          ES
+          <i data-feather="chevron-down" aria-hidden="true"></i>
+        </button>
+        <ul class="lang-menu dropdown">
+          <li><a value href="##">Ingles</a></li>
+          <li><a href="##">Español</a></li>
+          <li><a href="##">Portugues</a></li>
+        </ul>
+      </div>
+      <button class="theme-switcher gray-circle-btn" type="button" title="Cambiar Tema">
+        <span class="sr-only">Switch theme</span>
+        <i class="sun-icon" data-feather="sun" aria-hidden="true"></i>
+        <i class="moon-icon" data-feather="moon" aria-hidden="true"></i>
+      </button>
+      <div class="notification-wrapper">
+        <button class="gray-circle-btn dropdown-btn" title="Notificaciones" type="button">
+          <span class="sr-only">To messages</span>
+          <span class="icon notification active" aria-hidden="true"></span>
+        </button>
+        <ul class="users-item-dropdown notification-dropdown dropdown">
+          <li>
+            <a href="##">
+              <div class="notification-dropdown-icon info">
+                <i data-feather="check"></i>
+              </div>
+              <div class="notification-dropdown-text">
+                <span class="notification-dropdown__title">System just updated</span>
+                <span class="notification-dropdown__subtitle">The system has been successfully upgraded. Read more
+                  here.</span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="##">
+              <div class="notification-dropdown-icon danger">
+                <i data-feather="info" aria-hidden="true"></i>
+              </div>
+              <div class="notification-dropdown-text">
+                <span class="notification-dropdown__title">The cache is full!</span>
+                <span class="notification-dropdown__subtitle">Unnecessary caches take up a lot of memory space and
+                  interfere ...</span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="##">
+              <div class="notification-dropdown-icon info">
+                <i data-feather="check" aria-hidden="true"></i>
+              </div>
+              <div class="notification-dropdown-text">
+                <span class="notification-dropdown__title">New Subscriber here!</span>
+                <span class="notification-dropdown__subtitle">A new subscriber has subscribed.</span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a class="link-to-page" href="##">Go to Notifications page</a>
+          </li>
+        </ul>
+      </div>
+      <div class="nav-user-wrapper">
+        <button href="##" class="nav-user-btn dropdown-btn" title="Mi Perfil" type="button">
+          <span class="sr-only">Mi Perfil</span>
+          <span class="nav-user-img">
+            <picture><?php while ($row = mysqli_fetch_array($query2)): ?>
+            <img width="300px" src="<?=$row['url']?>" type="image/*">
+          <?php endwhile; ?></picture>
+          </span>
+        </button>
+        <ul class="users-item-dropdown nav-user-dropdown dropdown">
+          <li><a href="../perfiles/myPerfil.php">
+              <i data-feather="user" aria-hidden="true"></i>
+              <span>Perfil</span>
+            </a></li>
+          
+          <li><a class="danger" href="../cerrar-sesion/logout.php">
+              <i data-feather="log-out" aria-hidden="true"></i>
+              <span>Cerrar Sesion</span>
+            </a></li>
+        </ul>
+        
+      </div>
+    </div>
+    
+  </div>
+</nav>
---
+++ dashboard/sidebar/sidebar.php
@@ -0,0 +1,85 @@
+<aside class="sidebar">
+    <div class="sidebar-start">
+        <div class="sidebar-head">
+            <a href="../dashboard/home.php" class="logo-wrapper">
+                <span class="sr-only">Home</span>
+                <span class="icon logo" aria-hidden="true"></span>
+                <div class="logo-text">
+                    <span class="logo-title">Bill Bot</span>
+                    <span class="logo-subtitle">Administrador</span>
+                </div>
+
+            </a>
+            <button class="sidebar-toggle transparent-btn" title="Menu" type="button">
+                <span class="sr-only">Toggle menu</span>
+                <span class="icon menu-toggle" aria-hidden="true"></span>
+            </button>
+        </div>
+        <div class="sidebar-body">
+            <ul class="sidebar-body-menu">
+                <li>
+                    <a href="../dashboard/home.php"><span class="icon home" aria-hidden="true"></span>Dashboard</a>
+                </li>
+                <li>
+                    <a class="show-cat-btn" href="##">
+                        <span class="icon document" aria-hidden="true"></span>Armador
+                        <span class="category__btn transparent-btn" title="Open list">
+                            <span class="sr-only">Open list</span>
+                            <span class="icon arrow-down" aria-hidden="true"></span>
+                        </span>
+                    </a>
+                    <ul class="cat-sub-menu">
+                        <li>
+                            <a href="../dashboard/armador1.php">Armado masivo</a>
+                        </li>
+                        <li>
+                            <a href="../dashboard/armador2.php">Armado de cuentas</a>
+                        </li>
+                    </ul>
+                </li>
+                
+              
+                <li>
+                    <a href="../historial_acceso/historial.php">
+                        <span class="icon message" aria-hidden="true"></span>
+                        Comments
+                    </a>
+                </li>
+            </ul>
+            <span class="system-menu__title">SISTEMA</span>
+            <ul class="sidebar-body-menu">
+                <li>
+                    <a class="show-cat-btn" href="##">
+                        <span class="icon user-3" aria-hidden="true"></span>Gestion de perfil
+                        <span class="category__btn transparent-btn" title="Open list">
+                            <span class="sr-only">Open list</span>
+                            <span class="icon arrow-down" aria-hidden="true"></span>
+                        </span>
+                    </a>
+                    <ul class="cat-sub-menu">
+                        <li>
+                            <a href="../perfiles/funcionarios.php">Ver Perfiles</a>
+                        </li>
+            
+                    </ul>
+                </li>
+                <li>
+                    <a href="../video/info.php"><span class="icon setting" aria-hidden="true"></span>Configuracion</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="sidebar-footer">
+        <a href="mailto:montesdenzel25@gmail.com?subject=Solicitud%20de%20Soporte%20Tecnico" target='_blank' class="sidebar-user">
+            <span class="sidebar-user-img">
+                <picture class="bot"><source srcset="../img/bot.png" type="image/webp"><img src="./img/avatar/avatar-illustrated-01.png" alt="User name"></picture>
+            </span>
+            <div class="sidebar-user-info">
+                <span class="sidebar-user__title">Bill Bot</span>
+                <span class="sidebar-user__subtitle">Soporte tecnico</span>
+            </div>
+        </a>
+
+        
+    </div>
+</aside>
